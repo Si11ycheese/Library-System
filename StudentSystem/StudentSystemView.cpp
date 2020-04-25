1bbede5a10259acb25a5a9e32b0abe9b0016d513
志文 梁 <lzw2005@outlook.com>
2020-04-25 23:01:17 +0800

    添加项目文件。

---
+++ StudentSystem/StudentSystemView.cpp
@@ -0,0 +1,104 @@
+﻿
+// StudentSystemView.cpp: CStudentSystemView 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "StudentSystem.h"
+#endif
+
+#include "StudentSystemDoc.h"
+#include "StudentSystemView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CStudentSystemView
+
+IMPLEMENT_DYNCREATE(CStudentSystemView, CView)
+
+BEGIN_MESSAGE_MAP(CStudentSystemView, CView)
+	// 标准打印命令
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+END_MESSAGE_MAP()
+
+// CStudentSystemView 构造/析构
+
+CStudentSystemView::CStudentSystemView() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CStudentSystemView::~CStudentSystemView()
+{
+}
+
+BOOL CStudentSystemView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CStudentSystemView 绘图
+
+void CStudentSystemView::OnDraw(CDC* /*pDC*/)
+{
+	CStudentSystemDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CStudentSystemView 打印
+
+BOOL CStudentSystemView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 默认准备
+	return DoPreparePrinting(pInfo);
+}
+
+void CStudentSystemView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加额外的打印前进行的初始化过程
+}
+
+void CStudentSystemView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加打印后进行的清理过程
+}
+
+
+// CStudentSystemView 诊断
+
+#ifdef _DEBUG
+void CStudentSystemView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CStudentSystemView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CStudentSystemDoc* CStudentSystemView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CStudentSystemDoc)));
+	return (CStudentSystemDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CStudentSystemView 消息处理程序
---
+++ StudentSystem/StudentSystemView.h
@@ -0,0 +1,49 @@
+﻿
+// StudentSystemView.h: CStudentSystemView 类的接口
+//
+
+#pragma once
+
+
+class CStudentSystemView : public CView
+{
+protected: // 仅从序列化创建
+	CStudentSystemView() noexcept;
+	DECLARE_DYNCREATE(CStudentSystemView)
+
+// 特性
+public:
+	CStudentSystemDoc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 实现
+public:
+	virtual ~CStudentSystemView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // StudentSystemView.cpp 中的调试版本
+inline CStudentSystemDoc* CStudentSystemView::GetDocument() const
+   { return reinterpret_cast<CStudentSystemDoc*>(m_pDocument); }
+#endif
+
